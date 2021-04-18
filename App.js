48b06baf5011e9beac3686f051e3ada7e06da05a
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-19 00:55:22 +0300

    custom fonts added

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import "react-native-gesture-handler";
+import React from "react";
+import * as Font from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 // screens
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createStackNavigator();
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+const fonts = {
+  FuturaBold: require("./assets/fonts/Futura-Bold.ttf"),
+  LessRegular: require("./assets/fonts/Less-Regular.ttf"),
+  RalewayRegular: require("./assets/fonts/Raleway-Regular.ttf"),
+};
 
-export default App;
+export default class App extends React.Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(fonts);
+    this.setState({
+      fontsLoaded: true,
+    });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
