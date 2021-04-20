4772457d3af2e1e99064e8d543cbae468540854a
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-21 00:59:10 +0300

    swipeout component replaced with native swipeable

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,89 +1,98 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "../assets/styles";
-import Swipeout from "react-native-swipeout";
-import {
-  TouchableOpacity,
-  View,
-  Text,
-  Alert,
-  ToastAndroid,
-} from "react-native";
+import { View, Text, Alert, Animated, TouchableHighlight } from "react-native";
 import Feather from "react-native-vector-icons/Feather";
 import colors from "../assets/colors";
 import * as SQLite from "expo-sqlite";
-import * as Device from "expo-device";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
 const db = SQLite.openDatabase("database.db");
 
 Feather.loadFont();
 
 const TodoItem = ({ todo }) => {
-  const swipeLeft = [
-    {
-      text: <Feather name="check" size={34} color={colors.white} />,
-      backgroundColor: colors.green,
-      color: colors.white,
-      onPress: () => {
-        db.transaction((txn) => {
-          txn.executeSql(
-            "UPDATE todos SET is_done=? WHERE id=?",
-            [1, todo.id],
-            (tx, results) => {
-              if (Device.osName == "Android") {
-                ToastAndroid.showWithGravityAndOffset(
-                  `${todo.title} tamamlandı.`,
-                  ToastAndroid.LONG,
-                  ToastAndroid.TOP
-                );
-              }
-            }
-          );
-        });
-      },
-    },
-  ];
+  const doneTodo = () => {
+    db.transaction((txn) => {
+      txn.executeSql(
+        "UPDATE todos SET is_done=? WHERE id=?",
+        [1, todo.id],
+        (tx, results) => {
+          if (results.rowsAffected > 0) {
+            console.log("güncellendi");
+          }
+        }
+      );
+    });
+  };
 
-  const swipeRight = [
-    {
-      text: <Feather name="trash" size={34} color={colors.white} />,
-      backgroundColor: colors.red,
-      color: colors.white,
-      onPress: () => {
-        db.transaction((txn) => {
-          txn.executeSql(
-            "DELETE FROM todos WHERE id=?",
-            [todo.id],
-            (tx, results) => {
-              if (results.rowsAffected > 0) {
-                Alert.alert("Başarılı", "Todo başarıyla silindi", [
-                  {
-                    text: "Tamam",
-                    style: "cancel",
-                  },
-                ]);
-              }
-            }
-          );
-        });
-      },
-    },
-  ];
+  const deleteTodo = () => {
+    db.transaction((txn) => {
+      txn.executeSql(
+        "DELETE FROM todos WHERE id=?",
+        [todo.id],
+        (tx, results) => {
+          if (results.rowsAffected > 0) {
+            console.log("silindi");
+          }
+        }
+      );
+    });
+  };
+
+  const RightActions = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 140],
+      outputRange: [0, 1],
+    });
+
+    return (
+      <TouchableHighlight underlayColor="transparent" onPress={deleteTodo}>
+        <View style={styles.todoItemSwipe}>
+          <Animated.Text
+            style={{ transform: [{ scale }, { rotate: "180deg" }] }}
+          >
+            <Feather name="trash" size={32} color={colors.white} />
+          </Animated.Text>
+        </View>
+      </TouchableHighlight>
+    );
+  };
+
+  const LeftActions = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 140],
+      outputRange: [0, 1],
+    });
+
+    return (
+      <TouchableHighlight underlayColor="transparent" onPress={doneTodo}>
+        <View
+          style={[
+            styles.todoItemSwipe,
+            { backgroundColor: colors.green, marginLeft: 0, marginRight: 14 },
+          ]}
+        >
+          <Animated.Text style={{ transform: [{ scale }] }}>
+            <Feather name="check" size={32} color={colors.white} />
+          </Animated.Text>
+        </View>
+      </TouchableHighlight>
+    );
+  };
 
   return (
-    <Swipeout
-      left={swipeLeft}
-      right={swipeRight}
-      style={styles.todoItemSwipe}
-      autoClose={true}
+    <Swipeable
+      renderLeftActions={LeftActions}
+      renderRightActions={RightActions}
     >
-      <TouchableOpacity>
-        <View style={styles.todoItemWrapper}>
+      <TouchableHighlight>
+        <Animated.View style={[styles.todoItemWrapper]}>
           <Text style={styles.todoItemTitle}>{todo.title}</Text>
           <Text style={styles.todoItemSubtitle}>{todo.subtitle}</Text>
           <Text style={styles.todoItemContent}>{todo.content}</Text>
-        </View>
-      </TouchableOpacity>
-    </Swipeout>
+        </Animated.View>
+      </TouchableHighlight>
+    </Swipeable>
   );
 };
 
