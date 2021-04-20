df35cc29a3c9eab10aadabdd35ef162f5ad2ca8f
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-20 21:34:12 +0300

    added new colors

--- assets/colors/index.js
+++ assets/colors/index.js
@@ -1,10 +1,12 @@
 const colors = {
-    red: '#F03131',
-    green: '#21FF21',
-    black: '#2E2E2E',
-    grayDark: '#2E2E2E',
-    grayLight: '#DBDADA',
-    white: '#FFFFFF'
-}
+  red: "#F03131",
+  green: "#21FF21",
+  black: "#2E2E2E",
+  grayDark: "#2E2E2E",
+  grayLight: "#DBDADA",
+  white: "#FFFFFF",
+  pink: "#FFA4B0",
+  todoBackground: "#F7F7F7",
+};
 
 export default colors;
